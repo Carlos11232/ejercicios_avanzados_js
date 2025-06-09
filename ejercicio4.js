3863f9a4c5ddebd28fa2fab94fe26ce78a8a7121
Carlos11232 <carloscerezo212@gmail.com>
2025-06-09 10:43:03 +0200

    Update ejercicio4.js

--- ejercicio4.js
+++ ejercicio4.js
@@ -1,1 +1,25 @@
+function findArrayIndex(array, text) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === text) {
+      return i;
+    }
+  }
+  return -1; // Si no se encuentra el texto
+}
+
+const mainCharacters = [
+  "Luke",
+  "Leia",
+  "Han Solo",
+  "Chewbacca",
+  "Rey",
+  "Anakin",
+  "Obi-Wan",
+];
+
+// Ejemplos de uso:
+console.log(findArrayIndex(mainCharacters, "Luke"));       // 0
+console.log(findArrayIndex(mainCharacters, "Chewbacca"));  // 3
+console.log(findArrayIndex(mainCharacters, "Rey"));        // 4
+console.log(findArrayIndex(mainCharacters, "Yoda"));       // -1
 
